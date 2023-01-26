22991762d4ca382ba80c733f3001f6cfb64af6f9
hamidreza-abooei <hamidreza.abooei@gmail.com>
2023-01-26 17:26:28 -0500

    Add: First working server, Hello world

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
+	"net/http"
 
-
+	"github.com/labstack/echo/v4"
 )
 
-fucn main(){
+func main() {
+	e := echo.New()
 
+	e.GET("/hello", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "hello world")
+	})
 
-	
+	if err := e.Start("127.0.0.1:1358"); err != nil {
+		log.Fatalf("cannot start the echo http server: %s", err)
+	}
+
 }
